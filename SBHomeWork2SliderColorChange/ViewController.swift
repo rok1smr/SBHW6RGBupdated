b519ddd5b74afd3757ca37713cbee25e0e238a9c
ROK1SMR <runnerbkln@gmail.com>
2021-10-22 14:23:33 +0300

    Implemented the function of the red slider and some basic constraint to be able to run the app and test it Constraints have to be updated later

--- SBHomeWork2SliderColorChange/ViewController.swift
+++ SBHomeWork2SliderColorChange/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var view1: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var redSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.tintColor = .red
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.value = 0
+        view1.alpha = CGFloat(redSlider.value)
     }
 
-
+    @IBAction func redSliderAction(_ sender: Any) {
+        redTextField.text = String(format: "%.2f", Double(redSlider.value))
+        view1.alpha = CGFloat(redSlider.value)
+        view1.backgroundColor = .red
+    }
+    
 }
 
